eae433aff8352f2971aaf7680051600440a54d71
Lingbo Tang <lingbo19.tang@gmail.com>
2015-05-05 00:20:55 -0600

    asdf

---
+++ ArrayConstructor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	const int array_size = 10;
+	int ia[array_size];
+	for (int ix = 0; ix < array_size;++ix)
+		ia[ix] = ix;
+		cout << " This is it";
+	//return 0;
+}
